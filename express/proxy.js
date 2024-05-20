2dcf09ce62375b33ef92edc9038955ffc2e4cc63
Rivka Weingarten <rw240brooklyn@gmail.com>
2024-05-20 12:43:17 -0400

    one more try

--- express/proxy.js
+++ express/proxy.js
@@ -1,37 +1,48 @@
 const express = require('express');
 const axios = require('axios');
 const env = require('dotenv');
-const app = express();
-const PORT = 5000; // Choose a port for your proxy server
 const cors = require('cors');
+const path = require('path');
+
+env.config(); // Load environment variables from .env file as early as possible
+
+const app = express();
+const PORT = process.env.PORT || 5000; // Use the port from .env or default to 5000
 
 app.use(express.json());
 app.use(cors());
-// Load environment variables from .env file
-env.config();
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'src', 'build')));
+
+// Proxy endpoint to forward requests to the GraphQL API
 app.post('/graphql', async (req, res) => {
-    const { query, variables } = req.body;
-    const apiKey = process.env.REPORT_RESULTS_API_KEY;
-  
-    try {
-      const response = await axios.post(process.env.REPORT_RESULTS_API_ENDPOINT, {
-        query,
-        variables
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': apiKey
-        }
-      });
-  
-      res.json(response.data);
-      console.log(response.data)
-    } catch (error) {
-      console.error('Error proxying request:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  const { query, variables } = req.body;
+  const apiKey = process.env.REPORT_RESULTS_API_KEY;
+
+  try {
+    const response = await axios.post(process.env.REPORT_RESULTS_API_ENDPOINT, {
+      query,
+      variables
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': apiKey
+      }
+    });
+
+    res.json(response.data);
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error proxying request:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Catchall handler to serve React's index.html file for any unmatched routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'src', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Proxy server running on port ${PORT}`);
